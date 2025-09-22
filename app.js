48b530769f5ce63a82e55c6f4dc5989d8bcd76f0
Luka Fontanilla <maluka@google.com>
2025-09-22 19:24:18 +0000

    feat: adding simple cookieless example with vanilla js and no sdks

---
+++ app.js
@@ -0,0 +1,117 @@
+import express from 'express'
+import path from 'path'
+import cors from 'cors'
+import fs from 'fs'
+import user from './user.json' assert { type: 'json' }
+
+
+// Express App setup
+const app = express()
+app.use(cors())
+app.use(express.json())
+//
+
+// Simple in-memory object for managing session tokens
+let looker_tokens = {};
+//
+
+// Login to Looker API
+const getAdminToken = async () => {
+  const query = new URLSearchParams({
+    client_id: process.env.LOOKER_CLIENT_ID,
+    client_secret: process.env.LOOKER_CLIENT_SECRET,
+  });
+  const admin_access_token = await fetch(
+    `${process.env.LOOKER_API_URL}/api/4.0/login?${query.toString()}`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    }
+  ).then((response) => {
+    return response.json();
+  });
+  return admin_access_token;
+}
+//
+
+app.post('/api/acquire-embed-session', async (req, res) => {
+  try {
+    const admin_token = await getAdminToken();
+    if (!admin_token || !admin_token.access_token) {
+      return res.status(500).json({ message: "Failed to get admin access token" });
+    }
+
+    const embed_config = {
+      ...user,
+      session_reference_token: undefined,
+    };
+
+    const acquire = await fetch(
+      `${process.env.LOOKER_API_URL}/api/4.0/embed/cookieless_session/acquire`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${admin_token.access_token}`,
+          "User-Agent": req.headers['user-agent'],
+        },
+        body: JSON.stringify(embed_config),
+      }
+    ).then((response) => response.json());
+
+    const { session_reference_token, ...client_tokens } = acquire
+    looker_tokens = { session_reference_token, ...client_tokens }
+    res.json(client_tokens);
+  } catch (err) {
+    res.status(500).send({ message: err.message })
+  }
+})
+
+app.post('/api/generate-embed-tokens', async (req, res) => {
+  try {
+    const admin_token = await getAdminToken();
+    if (!admin_token || !admin_token.access_token) {
+      return res.status(500).json({ message: "Failed to get admin access token" });
+    }
+
+    const all = await fetch(
+      `${process.env.LOOKER_API_URL}/api/4.0/embed/cookieless_session/generate_tokens`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${admin_token.access_token}`,
+          "User-Agent": req.headers['user-agent'],
+        },
+        body: JSON.stringify({ ...gbl_tokens }),
+      }
+    ).then((response) => {
+      return response.json();
+    });
+
+    res.json(all);
+  } catch (err) {
+    res.status(500).send({ message: err.message })
+  }
+})
+
+app.get('/', (req, res) => {
+  fs.readFile(path.resolve('embed.html'), 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('An error occurred');
+    }
+    const result = data
+      .replace(/{{LOOKER_BASE_URL}}/g, process.env.LOOKER_BASE_URL)
+      .replace(/{{DASHBOARD_ID}}/g, process.env.LOOKER_DASHBOARD_ID);
+    res.send(result);
+  });
+});
+
+const PORT = process.env.PORT || 8000
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`)
+  console.log('Press Ctrl+C to quit.')
+})
